e385a90e7bbbaba0060e17aec63e6009628c9200
CHErnandez <carlos.hernandez@wolox.cl>
2018-07-20 14:18:23 -0400

    deleted local storage services

--- react-main/src/services/LocalStorageService.js
+++ react-main/src/services/LocalStorageService.js
@@ -3,7 +3,3 @@ export const saveKey = key => window.localStorage.setItem('token', key);
 export const getKey = () => window.localStorage.getItem('token');
 
 export const deleteKey = () => window.localStorage.removeItem('token');
-
-export const saveHistory = history => window.localStorage.setItem('gameHistory', history);
-
-export const getHistory = () => window.localStorage.getItem('gameHistory');
